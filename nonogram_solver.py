e4280cb874df377d3ee511716e35e4679b57342b
Carson F. Ball <carson.ball@gmail.com>
2020-12-14 13:58:02 -0600

    Fixed a number of PEP8 issues and combined the two matrix conversions into one call.

--- nonogram_solver.py
+++ nonogram_solver.py
@@ -1,15 +1,21 @@
 #! /usr/bin/python3
 
-def find_options( length, filled ):
-    total_filled = sum( filled )
+"""
+    This program will solve nonogram puzzles for any x*x size with given starting conditions.
+"""
+
+def find_options(length, filled):
+    """ This function finds all possibilities for the starting condition of the rows or columns. """
+
+    total_filled = sum(filled)
     total_empty = length - total_filled
-    extra = total_empty - len( filled ) + 1
+    extra = total_empty - len(filled) + 1
     lines = []
     ones = filled[0]
 
-    for pad in range( 0, extra + 1):
+    for pad in range(0, extra + 1):
         line = ''
-        blanks = len( filled )
+        blanks = len(filled)
         total_blanks = total_empty - pad
 
         line += '0' * pad
@@ -22,47 +28,103 @@ def find_options( length, filled ):
 
             total_blanks -= 1
 
-        if len( filled ) > 1:
-            subs = find_options( length - len( line ), filled[1:] )
+        if len(filled) > 1:
+            subs = find_options(length - len(line), filled[1:])
 
             for sub in subs:
-                lines.append( line + sub )
+                lines.append(line + sub)
         else:
             line += '0' * total_blanks
 
-            lines.append( line )
+            lines.append(line)
 
-    return( lines )
+    return lines
 
-def find_overlap( length, patterns ):
-    overlap = 0xFFFFFFFF;
+def find_overlap(length, patterns):
+    """ This function finds the overlap between the current possibilities for a row or column. """
+
+    overlap = 0xFFFFFFFF
 
     for pattern in patterns:
-        overlap &= int( pattern, base=2 )
+        overlap &= int(pattern, base=2)
 
-    return( "{0:b}".format( overlap ).zfill( length ) )
+    return "{0:b}".format(overlap).zfill(length)
 
-def compare_existing( length, patterns, existing=''):
+def compare_existing(length, patterns, existing=''):
+    """
+        This function adds information from a partially user-solved row or column to the
+        computer-generated row or column.
+    """
+
     if existing == '':
         existing = '0' * length
 
-    pattern = int( find_overlap( length, patterns ), base=2 )
+    pattern = int(find_overlap(length, patterns), base=2)
 
-    pattern |= int( existing, base=2 )
+    pattern |= int(existing, base=2)
 
-    return( "{0:b}".format( pattern ).zfill( length ) )
+    return "{0:b}".format(pattern).zfill(length)
 
-def update_existing( from_existing, to_existing ):
-    for row_index in range(0, len( from_existing ) ):
-        for col_index in range(0, len( to_existing ) ):
-            if from_existing[row_index][col_index] == '1':
-                to_existing[col_index] = to_existing[col_index][:row_index] + '1' + to_existing[col_index][row_index+1:]
+def update_existing(col_existing, row_existing):
+    """
+        This function updates the rows based on the columns and vice-versa.  It basically rotates
+        one matrix in memory and compares it to the other one.
+    """
 
-    return( to_existing )
+    for row_index in range(0, len(col_existing)):
+        for col_index in range(0, len(row_existing)):
+            if col_existing[row_index][col_index] == '1':
+                row_existing[col_index] \
+                    = row_existing[col_index][:row_index] \
+                    + '1' \
+                    + row_existing[col_index][row_index+1:]
 
-length = 15
+            if row_existing[row_index][col_index] == '1':
+                col_existing[col_index] \
+                    = col_existing[col_index][:row_index] \
+                    + '1' \
+                    + col_existing[col_index][row_index+1:]
 
-horizontal_grid = [
+    return row_existing, col_existing
+
+def solve(length, horizontal_grid, vertical_grid):
+    """
+        This function will solve a nonogram given the length, horizontal_grid, and vertical_grid.
+    """
+
+    horizontal_existing = []
+    vertical_existing = []
+
+    horizontal_backup = []
+    vertical_backup = []
+
+    for row in horizontal_grid:
+        patterns = find_options(length, row)
+
+        horizontal_existing.append(find_overlap(length, patterns))
+
+    for row in vertical_grid:
+        patterns = find_options(length, row)
+
+        vertical_existing.append(find_overlap(length, patterns))
+
+    done = 0
+
+    while not done:
+        horizontal_backup = horizontal_existing[:]
+        vertical_backup = vertical_existing[:]
+
+        vertical_existing, horizontal_existing \
+            = update_existing(horizontal_existing, vertical_existing)
+
+        if (horizontal_existing == horizontal_backup and vertical_existing == vertical_backup):
+            done = 1
+
+    return horizontal_existing
+
+LENGTH = 15
+
+HORIZONTAL_GRID = [
     [3,2],
     [1,4,1],
     [2],
@@ -80,7 +142,7 @@ horizontal_grid = [
     [5,4]
 ]
 
-vertical_grid = [
+VERTICAL_GRID = [
     [2,2],
     [1,1,3],
     [2,2,5,1],
@@ -98,33 +160,7 @@ vertical_grid = [
     [5]
 ]
 
-horizontal_existing = []
-vertical_existing = []
+# TODO: Keep track of the number of passes.
 
-horizontal_backup = []
-vertical_backup = []
-
-for row in horizontal_grid:
-    patterns = find_options( length, row )
-
-    horizontal_existing.append( find_overlap( length, patterns ) )
-
-for row in vertical_grid:
-    patterns = find_options( length, row )
-
-    vertical_existing.append( find_overlap( length, patterns ) )
-
-done = 0
-
-while (not done):
-    horizontal_backup = horizontal_existing[:]
-    vertical_backup = vertical_existing[:]
-
-    vertical_existing   = update_existing( horizontal_existing, vertical_existing   )
-    horizontal_existing = update_existing( vertical_existing,   horizontal_existing )
-
-    if (horizontal_existing == horizontal_backup and vertical_existing == vertical_backup):
-        done = 1
-
-for row in horizontal_existing:
-    print( row )
+for solved in solve(LENGTH, HORIZONTAL_GRID, VERTICAL_GRID):
+    print(solved)
